481c4e9bc80769c65dce65a8a403d5de87b19fb2
Divine-cell <106761608+Divine-cell@users.noreply.github.com>
2023-04-23 13:27:12 -0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+//let countEL = document.getElementById("count-el")
+//let count = 0
+
+//function increment() {
+//   count += 1
+//   countEL.textContent = count
+//}
+
+let countEl = document.getElementById("count-el")
+        let saveEl = document.getElementById("save-el")
+        let count = 0
+
+        function increment() {
+            count += 1
+            countEl.textContent = count
+        }
+
+        function save() {
+         saveEl.textContent += count + "-"
+         countEl.textContent = 0
+         count = 0
+        
+        }
